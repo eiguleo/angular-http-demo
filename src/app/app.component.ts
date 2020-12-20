fd2818382be21b6f59a5159b3dda1b659e1718dd
U-DESKTOP-LFIDO1U\huzh <eiguleo@126.com>
2020-12-20 19:01:05 +0800

    增加按钮

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+interface Todo {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: "./app.component.html",
+  // `
+  //   <ul *ngIf="todos$ | async as todos; else noData">
+  //     <li *ngFor="let todo of todos">
+  //       <span>Title: {{ todo.id }}</span> ——
+  //       <span>Completed: {{ todo.name }}</span>
+  //     </li>
+  //   </ul>
+  //   <ng-template #noData>No Data Available</ng-template>
+  // `,
 })
-export class AppComponent {
-  title = 'angular-http-example';
+export class AppComponent implements OnInit {
+  todos$: Observable<Todo[]>;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // this.todos$ = this.http
+    //   .get<Todo[]>(
+    //     // 'https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10'
+    //     'http://127.0.0.1:8080/ping'
+    //   )
+      // .pipe(tap(console.log));
+  }
+
+  getlist(){
+    this.todos$ = this.http
+    .get<Todo[]>(
+      // 'https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10'
+      'http://127.0.0.1:8080/ping'
+    ).pipe(tap(console.log));
+  }
 }
